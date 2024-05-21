746a5973584b48651adf1889d81debee1ad3dc37
Bob <esdrassantos41@gmail.com>
2024-05-21 00:39:32 -0300

    projeto calculadora de entregas

---
+++ public/script.js
@@ -0,0 +1,29 @@
+// /public/script.js
+document.getElementById('uploadForm').addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    const fileField = document.querySelector('input[type="file"]');
+    formData.append('pdfFile', fileField.files[0]);
+
+    try {
+        const response = await fetch('/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const result = await response.json();
+        document.getElementById('result').innerText = `Custo total de entrega: R$${result.total}`;
+
+       // Exibir os pesos no console do navegador
+       const weights = result.weights; // Supondo que o servidor agora retorna os pesos juntamente com o total
+       console.log('Lista de pesos:', weights);
+
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        document.getElementById('result').innerText = 'An error occurred while processing the PDF.';
+    }
+});
---
+++ server.js
@@ -0,0 +1,51 @@
+// /server.js
+const express = require('express');
+const multer = require('multer');
+const pdf = require('pdf-parse');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+// Serve arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/upload', upload.single('pdfFile'), async (req, res) => {
+    try {
+        const dataBuffer = fs.readFileSync(req.file.path);
+        const data = await pdf(dataBuffer);
+
+        const lines = data.text.split('\n');
+        let total = 0;
+        let weights = [];
+
+        for (const line of lines) {
+            const match = line.match(/tx([\d.,]+)/);
+            if (match) {
+                const weight = parseFloat(match[1].replace(',', '.'));
+                weights.push(weight);
+                if (weight > 0.3) {
+                    total += 3;
+                } else {
+                    total += 2;
+                }
+            }
+        }
+
+        res.json({ total, weights });
+    } catch (error) {
+        console.error('Erro ao processar o arquivo', error);
+        res.status(500).json({ error: 'Erro ao processar o arquivo' });
+    } finally {
+        fs.unlink(req.file.path, (err) => {
+            if (err) console.error(`Erro ao deletar o arquivo: ${err}`);
+        });
+    }
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
